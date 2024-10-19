310a0dec5221820b89bfb129a9c73bdf3ae1f662
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-10-19 11:08:17 -0500

    Vercel going to feel this one

--- extend/src/routes/dash/+page.svelte
+++ extend/src/routes/dash/+page.svelte
@@ -1,6 +1,6 @@
 <script>
   import { onMount } from 'svelte';
-  import { user } from '../../authStore';
+  import { user } from '../../authstore';
   import { goto } from '$app/navigation';
   import { geminiInference } from '../../geminiInference';
   import { db } from '../../firebase';
